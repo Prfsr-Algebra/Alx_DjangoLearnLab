9f8e4811a0b5aee0d5d25b4b4022130a53657083
Prfsr-Algebra <himsluve@gmail.com>
2025-03-18 00:17:50 -0700

    fixed 5

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.filters import OrderingFilter
 
 from rest_framework import generics
 from .models import Book
